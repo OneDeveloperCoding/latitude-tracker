bd5ad7f457bdcef2c0b7aa2e12b712d81438076b
OneDeveloperCoding <onedevelopercoding@gmail.com>
2026-06-01 20:14:57 +0100

    Add Firebase Crashlytics for automatic crash reporting

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
@@ -8,6 +11,14 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
   await LocaleSettings.init();
-  runApp(const LatitudeTrackerApp());
+
+  runZonedGuarded(
+    () => runApp(const LatitudeTrackerApp()),
+    (error, stack) =>
+        FirebaseCrashlytics.instance.recordError(error, stack, fatal: true),
+  );
 }
