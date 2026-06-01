74f6252cf0e5a3b6ea6d86ab573424b6cda1d871
OneDeveloperCoding <onedevelopercoding@gmail.com>
2026-06-01 20:24:33 +0100

    Fix zone mismatch in main and hero tag collision on FABs

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,15 @@ import 'app.dart';
 import 'core/l10n/locale_settings.dart';
 import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+void main() {
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
 
-  await LocaleSettings.init();
+    await LocaleSettings.init();
 
-  runZonedGuarded(
-    () => runApp(const LatitudeTrackerApp()),
-    (error, stack) =>
-        FirebaseCrashlytics.instance.recordError(error, stack, fatal: true),
-  );
+    runApp(const LatitudeTrackerApp());
+  }, (error, stack) => FirebaseCrashlytics.instance.recordError(error, stack, fatal: true));
 }
