debec9a03739dd30c00f95161adc6eb636777bd1
OneDeveloperCoding <onedevelopercoding@gmail.com>
2026-06-04 18:40:27 +0100

    fix: close two gaps in Repair feature found during review
    - Add deleteAllRepairs() to RepairRepository interface + Firestore + InMemory impls
      so ResetAppService can wipe repairs on full app reset
    - Wire RepairRepository.deleteAllRepairs() into ResetAppService.resetApp()
    - Add insights icon button to RepairsListScreen AppBar that opens AnalyticsScreen
      pre-navigated to the Repairs tab (startOnRepairs: true)

--- lib/features/settings/services/reset_app_service.dart
+++ lib/features/settings/services/reset_app_service.dart
@@ -1,14 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 import '../../buyers/repositories/buyer_repository.dart';
+import '../../repairs/repositories/repair_repository.dart';
 import '../../sales/repositories/sale_repository.dart';
 
 class ResetAppService {
   final _salesRepo = SaleRepository();
   final _buyersRepo = BuyerRepository();
+  final _repairsRepo = RepairRepository();
 
   Future<void> resetApp({bool deletePhotos = false}) async {
     await _salesRepo.deleteAllSales(deletePhotos: deletePhotos);
+    await _repairsRepo.deleteAllRepairs(deletePhotos: deletePhotos);
     await _buyersRepo.deleteAllBuyers();
     await FirebaseAuth.instance.signOut();
   }
